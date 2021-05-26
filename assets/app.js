d0d3adf8fcdb22491e1e5d6462be567f1b143dce
Mehdi <mehdi693@live.fr>
2021-05-26 17:10:42 +0200

    anime.js

--- assets/app.js
+++ assets/app.js
@@ -10,3 +10,35 @@ import './styles/app.css';
 
 // start the Stimulus application
 import './bootstrap';
+
+import anime from 'animejs/lib/anime.es.js';
+
+
+
+window.addEventListener('scroll', reveal);
+
+function reveal() {
+    let element = document.querySelector('.subheading');
+
+    let position = element.getBoundingClientRect();
+
+    if (position.top >= 0 && position.bottom <= window.innerHeight) {
+        element.classList.add('logo_animation')
+    }
+}
+
+let typed = new Typed('#typed', {
+    strings: ['UX Design', 'UI Design', 'Developpeur', 'Web', 'Dev', 'Front', 'Data'],
+    typeSpeed: 0,
+    backSpeed: 0,
+    smartBackspace: true, // c'est par defaut
+    loop: true
+});
+
+anime({
+    targets: '#yo',
+    translateX: 250,
+    rotate: '1turn',
+    backgroundColor: '#FFF',
+    duration: 800
+});
